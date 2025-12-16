79623e4f5ff8cb7b42e34bf59fe9a4eb54fbd90a
secret_online <me@secretonline.co>
2025-12-17 10:37:45 +1300

    Create dynamic registry

--- src/client/java/co/secretonline/dynregtest/DynamicRegistryTestClient.java
+++ src/client/java/co/secretonline/dynregtest/DynamicRegistryTestClient.java
@@ -1,10 +1,15 @@
 package co.secretonline.dynregtest;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientWorldEvents;
 
 public class DynamicRegistryTestClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		ClientWorldEvents.AFTER_CLIENT_WORLD_CHANGE.register((minecraft, level) -> {
+			level.registryAccess().get(DynamicRegistryTest.REGISTRY_KEY).ifPresentOrElse(
+					registry -> DynamicRegistryTest.LOGGER.info("Client: registry has {} values", registry.value().size()),
+					() -> DynamicRegistryTest.LOGGER.warn("Client: registry not found"));
+		});
 	}
 }
--- src/main/java/co/secretonline/dynregtest/DynamicRegistryTest.java
+++ src/main/java/co/secretonline/dynregtest/DynamicRegistryTest.java
@@ -1,24 +1,39 @@
 package co.secretonline.dynregtest;
 
-import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
+import net.fabricmc.fabric.api.event.registry.DynamicRegistries;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
 
 public class DynamicRegistryTest implements ModInitializer {
 	public static final String MOD_ID = "dynamic-registry-test";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static ResourceKey<Registry<TestValue>> REGISTRY_KEY = ResourceKey
+			.createRegistryKey(Identifier.fromNamespaceAndPath(DynamicRegistryTest.MOD_ID, "test_registry"));
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		DynamicRegistries.registerSynced(REGISTRY_KEY, TestValue.CODEC);
 
-		LOGGER.info("Hello Fabric world!");
+		ServerWorldEvents.LOAD.register((minecraft, level) -> {
+			level.registryAccess().get(REGISTRY_KEY).ifPresentOrElse(
+					registry -> LOGGER.info("Server: registry has {} values", registry.value().size()),
+					() -> LOGGER.warn("Server: registry not found"));
+		});
+	}
+
+	public record TestValue(Identifier id) {
+		public static Codec<TestValue> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+				Identifier.CODEC.fieldOf("id").forGetter(TestValue::id)).apply(instance, TestValue::new));
 	}
 }
