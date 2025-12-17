8a9862fa83a446cbec3acade2cedbf8ee943a948
secret_online <me@secretonline.co>
2025-12-18 11:26:06 +1300

    Print on more events

--- src/client/java/co/secretonline/dynregtest/DynamicRegistryTestClient.java
+++ src/client/java/co/secretonline/dynregtest/DynamicRegistryTestClient.java
@@ -2,14 +2,26 @@ package co.secretonline.dynregtest;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientWorldEvents;
+import net.fabricmc.fabric.api.event.registry.DynamicRegistrySetupCallback;
+import net.minecraft.resources.Identifier;
 
 public class DynamicRegistryTestClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		ClientWorldEvents.AFTER_CLIENT_WORLD_CHANGE.register((minecraft, level) -> {
 			level.registryAccess().get(DynamicRegistryTest.REGISTRY_KEY).ifPresentOrElse(
-					registry -> DynamicRegistryTest.LOGGER.info("Client: registry has {} values", registry.value().size()),
-					() -> DynamicRegistryTest.LOGGER.warn("Client: registry not found"));
+					registry -> DynamicRegistryTest.LOGGER.info("Client world change: registry has {} values",
+							registry.value().size()),
+					() -> DynamicRegistryTest.LOGGER.warn("Client world change: registry not found"));
+		});
+		DynamicRegistrySetupCallback.EVENT.register((view) -> {
+			view.registerEntryAdded(DynamicRegistryTest.REGISTRY_KEY,
+					(int rawId, Identifier id, DynamicRegistryTest.TestValue object) -> DynamicRegistryTest.LOGGER
+							.info("Client registering: {}", id.toString()));
+
+			view.getOptional(DynamicRegistryTest.REGISTRY_KEY).ifPresentOrElse(
+					registry -> DynamicRegistryTest.LOGGER.info("Client registry setup: registry has {} values", registry.size()),
+					() -> DynamicRegistryTest.LOGGER.warn("Client registry setup: registry not found"));
 		});
 	}
 }
--- src/main/java/co/secretonline/dynregtest/DynamicRegistryTest.java
+++ src/main/java/co/secretonline/dynregtest/DynamicRegistryTest.java
@@ -7,8 +7,10 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
 import net.fabricmc.fabric.api.event.registry.DynamicRegistries;
+import net.fabricmc.fabric.api.event.registry.DynamicRegistrySetupCallback;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
@@ -21,14 +23,40 @@ public class DynamicRegistryTest implements ModInitializer {
 	public static ResourceKey<Registry<TestValue>> REGISTRY_KEY = ResourceKey
 			.createRegistryKey(Identifier.fromNamespaceAndPath(DynamicRegistryTest.MOD_ID, "test_registry"));
 
+	private long tickNumber = 0;
+
+	static {
+		DynamicRegistries.registerSynced(REGISTRY_KEY, TestValue.CODEC);
+	}
+
 	@Override
 	public void onInitialize() {
-		DynamicRegistries.registerSynced(REGISTRY_KEY, TestValue.CODEC);
 
 		ServerWorldEvents.LOAD.register((minecraft, level) -> {
 			level.registryAccess().get(REGISTRY_KEY).ifPresentOrElse(
-					registry -> LOGGER.info("Server: registry has {} values", registry.value().size()),
-					() -> LOGGER.warn("Server: registry not found"));
+					registry -> LOGGER.info("Server load: registry has {} values", registry.value().size()),
+					() -> LOGGER.warn("Server load: registry not found"));
+		});
+
+		ServerTickEvents.END_WORLD_TICK.register((level) -> {
+			tickNumber++;
+			if (tickNumber != 100) {
+				return;
+			}
+
+			level.registryAccess().get(REGISTRY_KEY).ifPresentOrElse(
+					registry -> LOGGER.info("Server 100th tick: registry has {} values", registry.value().size()),
+					() -> LOGGER.warn("Server 100th tick: registry not found"));
+		});
+
+		DynamicRegistrySetupCallback.EVENT.register((view) -> {
+			view.registerEntryAdded(DynamicRegistryTest.REGISTRY_KEY,
+					(int rawId, Identifier id, DynamicRegistryTest.TestValue object) -> DynamicRegistryTest.LOGGER
+							.info("Server registering: {}", id.toString()));
+
+			view.getOptional(DynamicRegistryTest.REGISTRY_KEY).ifPresentOrElse(
+					registry -> DynamicRegistryTest.LOGGER.info("Server registry setup: registry has {} values", registry.size()),
+					() -> DynamicRegistryTest.LOGGER.warn("Server registry setup: registry not found"));
 		});
 	}
 
